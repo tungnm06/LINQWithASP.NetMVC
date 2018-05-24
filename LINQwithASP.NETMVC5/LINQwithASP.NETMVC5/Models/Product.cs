9c2ab87035240505fe5b8b7cc87641c373784b21
tungnm06 <tungnmd00439@fpt.edu.vn>
2018-05-24 11:06:50 +0700

    LINQ MVC

---
+++ LINQwithASP.NETMVC5/LINQwithASP.NETMVC5/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LINQwithASP.NETMVC5.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Qty { get; set; }
+        public decimal Prince { get; set; }
+        public DateTime EntryDate { get; set; }
+
+    }
+}
